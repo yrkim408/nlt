8001c72bb8eeab1cd5b1aeaf695cee7ddb07872a
yrkim408 <yek003@ucsd.edu>
2024-11-26 10:50:00 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+// App.js
 import './App.css';
+import React from 'react';
+import KommunicateChat from './chat';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My Chat App</h1>
       </header>
+      <main>
+        <p>This is the main content area</p>
+        <KommunicateChat/>
+      </main>
     </div>
   );
 }
